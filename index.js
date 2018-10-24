0b2a3184bb4d9c41507e267e6ae66251ac9e17ba
Mikeon <mbdotdev@gmail.com>
2018-10-24 20:53:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var os = require('os');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World from App Service");
+
+    // Stage 2
+    // response.end('Hello from ' + process.env.Lesson + ' from server: ' + os.hostname() + " with OS: " + os.platform());
+
+});
+
+var port = process.env.PORT || 8080;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
