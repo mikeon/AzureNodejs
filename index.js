f36b7c4e1a5a760385c997923dcaa869f1c637c8
Michał Bortnowski (MIBN) <mibn@dgs.com>
2019-01-11 12:26:52 +0100

    Changed response

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var os = require('os');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World from App Service");
+    // response.end("Hello World from App Service");
     response.end('Hello test from ' + process.env.Lesson + ' server: ' + os.hostname() + " with OS: " + os.platform());
 });
 
